71fa0311c81baebb8f6e296bd7975bcfa89ef02e
Sarrene Madi <p15006393@student.newinti.edu.my>
2022-05-21 21:33:39 +0800

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\AdventureComponent;
+use App\Http\Livewire\RomanceComponent;
+use App\Http\Livewire\ChildrenComponent;
+use App\Http\Livewire\SciFiComponent;
+use App\Http\Livewire\AdminAddBookComponent;
+use App\Http\Livewire\MysteryComponent;
+use App\Http\Livewire\HorrorComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,17 +19,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/adventure', AdventureComponent::class);
+Route::get('/romance', RomanceComponent::class);
+Route::get('/children', ChildrenComponent::class);
+Route::get('/sci-fi', SciFiComponent::class);
+Route::get('admin-addbook', AdminAddBookComponent::class)->name('addbook');
+Route::get('/mystery', MysteryComponent::class);
+Route::get('/horror', HorrorComponent::class);
 
-Route::get('/', function () {
-    return view('welcome');
-
-});
-
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-});
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
